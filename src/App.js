430b8ac72485e61b3326a36eb1582c3d61b59e7a
dgatere <dennisgatere@gmail.com>
2019-08-14 19:56:44 +0300

    Changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learning React
+					Feature branch change
 				</a>
 			</header>
 		</div>
